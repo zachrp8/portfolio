d8ca12ce81352647cf491b01aa4dd6d5b3c95991
Zach Parkhurst <zparkhurst11@gmail.com>
2023-07-08 17:30:45 -0400

    Updated to match myself

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,7 +5,7 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
+    title: 'Zach Parkhurst',
     site_url: 'https://yoursite.com',
     feed_url: 'https://yoursite.com/feed.xml',
   })
